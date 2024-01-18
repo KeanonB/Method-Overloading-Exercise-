07aedbce09c2767f16bcdb278b999a7b02d7a366
Keanon Born <disposingmoon1@gmail.com>
2024-01-18 15:53:09 -0500

    final

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,36 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(6, 6));
+            Console.WriteLine(Add(2.1m, 1.7m));
+            Console.WriteLine(Add(1,0,true));
+            
         }
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public static decimal Add(decimal num1, decimal num2)
+        {
+            return num1 + num2;
+        }
+        public static string Add(int x, int y, bool isTrue)
+        {
+
+            if (isTrue == true && (x + y) > 1)
+            {
+                return $"{x + y} dollars";
+            }
+            else if (isTrue == true && (x + y) == 1)
+            {
+                return $"{x + y} dollar";
+
+            }
+            else
+            {
+                return $"{x + y} dollars";
+            }
+        }
+
     }
 }
